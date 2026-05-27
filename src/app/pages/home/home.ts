ce8ead68be8e92b71ef046aefd0fabe82cb0ddb2
JORDYROJAS93 <jordy_r12@hotmail.com>
2026-05-26 21:40:31 -0500

    cambios para mejorar el navbar

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+
+  // 2. Inyéctalo en tu constructor
+  constructor(private router: Router) {}
+
+  // 3. Añade esta función mágica que forzará la navegación al hacer clic
+  irA(ruta: string): void {
+    this.router.navigate([ruta]);
+  }
+}
