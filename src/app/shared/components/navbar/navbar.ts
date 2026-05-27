0ce1dc80a91af5e046eae4ec5ce30b9c789cbb16
JORDYROJAS93 <jordy_r12@hotmail.com>
2026-05-26 20:04:37 -0500

    cambios para mejorar el navbar

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -9,15 +9,32 @@ import { RouterLink, RouterLinkActive,  } from '@angular/router';
   styleUrl: './navbar.css',
 })
 export class NavbarComponent {
-  // Estado para el menú hamburguesa en móviles
   menuAbierto: boolean = false;
+  
+  // Guarda el nombre de la categoría móvil abierta ('finanzas', 'marketing', etc.)
+  // Si es null, todas las categorías están cerradas.
+  categoriaActiva: string | null = null;
 
   toggleMenu() {
     this.menuAbierto = !this.menuAbierto;
+    // Al cerrar el menú completo, limpiamos también las categorías abiertas
+    if (!this.menuAbierto) {
+      this.categoriaActiva = null;
+    }
   }
 
   cerrarMenu() {
     this.menuAbierto = false;
+    this.categoriaActiva = null;
+  }
+
+  // Lógica del Acordeón móvil
+  toggleCategoria(categoria: string) {
+    if (this.categoriaActiva === categoria) {
+      this.categoriaActiva = null; // Si hace clic en la misma, se cierra
+    } else {
+      this.categoriaActiva = categoria; // Abre la nueva y cierra la anterior automáticamente
+    }
   }
 
 }
